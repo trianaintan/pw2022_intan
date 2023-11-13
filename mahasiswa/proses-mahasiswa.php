c1a7895dbe029539028cc2812ce4e60a9399a322
triana intan <trianaintan30@gmail.com>
2023-11-13 10:48:57 +0700

    ada penambahan dan perubahan file

--- mahasiswa/proses-mahasiswa.php
+++ mahasiswa/proses-mahasiswa.php
@@ -2,28 +2,36 @@
 session_start();
 require_once "../config.php";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['update'])) {
-    $nim = $_POST["nim"];
-    $nm_mahasiswa = htmlspecialchars($_POST["nm_mahasiswa"]);
-    $alamat = htmlspecialchars($_POST["alamat"]);
-    $no_telfon = $_POST["no_telfon"];
+if (isset($_POST['simpan'])) {
+    $nim = $_POST['nim'];
+    $nm_mahasiswa = htmlspecialchars($_POST['nm_mahasiswa']);
+    $alamat = htmlspecialchars($_POST['alamat']);
+    $no_telfon = $_POST['no_telfon'];
 
-    $stmt = mysqli_prepare($koneksi, "UPDATE mahasiswa SET nm_mahasiswa = ?, alamat = ?, no_telfon = ? WHERE nim = ?");
-    mysqli_stmt_bind_param($stmt, "ssss", $nm_mahasiswa, $alamat, $no_telfon, $nim);
+    $stmt = mysqli_prepare($koneksi, "INSERT INTO mahasiswa VALUES (?, ?, ?, ?)");
+    mysqli_stmt_bind_param($stmt, "ssss", $nim, $nm_mahasiswa, $alamat, $no_telfon);
 
     if (mysqli_stmt_execute($stmt)) {
         echo "<script>
-                alert('Data berhasil diupdate');
-                window.location.href ='mahasiswa.php';
-              </script>";
+                alert('Data berhasil disimpan');
+                document.location.href ='mahasiswa.php';
+            </script>";
     } else {
         echo "<script>
-                alert('Data gagal diupdate');
-              </script>";
+                alert('Data gagal disimpan');
+            </script>";
     }
 
     mysqli_stmt_close($stmt);
-} elseif ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['simpan'])) {
-    // Logika untuk menyimpan data baru, jika diperlukan
+    return;
+} else if (isset($_POST['update'])) {
+    $nim = $_POST["nim"];
+    $nm_mahasiswa = $_POST["nm_mahasiswa"];
+    $alamat = $_POST["alamat"];
+    $no_telfon = $_POST["no_telfon"];
+    // Handle update as shown in the update-mahasiswa.php file
+    // ...
+    // Ensure that you are using prepared statements for security
+    // ...
 }
 ?>
